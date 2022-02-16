e5265d2729af6dc562cb3bad906da33491ee3e28
Valentin <piterans@warp.is>
2022-02-16 20:02:19 +0500

    Governance sites added

--- app/gov_prop/loader.py
+++ app/gov_prop/loader.py
@@ -1,6 +1,8 @@
 import json
+from datetime import datetime, timedelta
 
 import requests
+from lxml import html
 
 source_list_path = "app/gov_prop/sources.json"
 sources = json.loads(open(source_list_path, "r").read())
@@ -13,20 +15,44 @@ headers = {
 
 class GovernanceAndProposals:
     def get_new_gov_topics(self, _response):
-        pass
+        news = []
+        now = datetime.now().date()
+        if _response.status_code == 200:
+            root = html.fromstring(_response.text)
+            topics = root.xpath("//tr[@class='topic-list-item']")
+            for topic in topics:
+                header = topic.xpath(".//a[@class='title raw-link raw-topic-link']/text()")[0]
+                date = topic.xpath(".//td[last()]/text()")[0].strip()
+                try:
+                    created_at = datetime.strptime(date, "%B %d, %Y").date()
+                except ValueError:
+                    created_at = datetime.strptime(date, "%d %B %Y").date()
+                if now - created_at < timedelta(days=1):
+                    news.append([header, created_at])
+        return news
 
     def get_new_prop_topics(self, _response):
-        pass
+        news = []
+        now = datetime.now().date()
+        if _response.status_code == 200:
+            root = html.fromstring(_response.text)
+            topics = root.xpath("//div[@class='border-t border-b md:border rounded-none md:rounded-lg mb-4 bg-skin-block-bg timeline-proposal transition-colors']")
+
 
     def send_news_to_tg(self, _messages):
         pass
 
     def get_news(self):
-        new_topics = []
-        for name, source in sources:
+        new_topics = {}
+        for name, source in sources.items():
             if "gov" in source:
                 response = requests.get(f"{source[0]}/latest", headers=headers)
-                new_topics.append(self.get_new_gov_topics(response))
+                new_topics[name] = self.get_new_gov_topics(response)
             elif "prop" in source:
                 response = requests.get(f"{source[0]}/#/", headers=headers)
-                new_topics.append(self.get_new_prop_topics(response))
+                new_topics[name] = self.get_new_prop_topics(response)
+
+
+def start():
+    parser = GovernanceAndProposals()
+    parser.get_news()
