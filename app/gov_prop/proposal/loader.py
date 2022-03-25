6b8d5160d8239e09c1b5e5ad4b64aae5000c250f
Valentin <piterans@warp.is>
2022-03-25 19:30:12 +0500

    change css selectors for proposals

--- app/gov_prop/proposal/loader.py
+++ app/gov_prop/proposal/loader.py
@@ -1,6 +1,6 @@
 import logging
 
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
@@ -22,21 +22,19 @@ class ProposalNews:
             new_proposals = []
             status = ""
             while status != "Closed":
-                logger.info(self.url)
                 wait = WebDriverWait(driver, 100)
                 wait.until(EC.element_to_be_clickable((By.XPATH, "//span[@class='truncate w-full']")))
-                containers = page.find_elements(By.XPATH, "//div[@class='leading-6']")
+                containers = page.find_elements(By.XPATH, "//div[@class='leading-5 sm:leading-6']")
                 for container in containers[1:]:
                     try:
                         status = container.find_element(By.CLASS_NAME, "State").text
-                    except NoSuchElementException:
+                    except (NoSuchElementException, StaleElementReferenceException):
                         continue
-                    if status != "Closed":
+                    if status != "Closed" or status == "":
                         try:
-                            text = container.find_element(By.XPATH, "//p[@class='break-words mb-2 text-md']").text
+                            text = container.find_element(By.XPATH, "//p[@class='break-words mb-2 sm:text-md']").text
                         except NoSuchElementException:
                             text = ""
-                        logger.info(f"{text = }")
                         try:
                             header = container.find_element(By.CLASS_NAME, "my-1").text
                         except NoSuchElementException:
